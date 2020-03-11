a0630c36f98d90f9b510c26e36ac0486af002d88
yoonahChoi <ejdnj9710@gamil.com>
2020-03-11 17:10:12 +0900

    InputBox 구현

---
+++ todolist/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <header class="header"><h1>ToDoList</h1></header>
+    <input-box v-bind:value="newItem" v-on:@addItem="addItem"></input-box>
+  </div>
+</template>
+
+<script>
+import InputBox from './components/InputBox.vue'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      newItem: ''
+    }
+  },
+  components: {
+    'InputBox' : InputBox,
+  },
+  methods: {
+    addItem(item) {
+      this.newItem = item
+      if(this.newItem !=='') {
+        let value = this.newItem && this.newItem.trim()
+        localStorage.setItem(value, value)
+      }
+    },
+  }
+}
+</script>
+<style>
+.header{
+  text-align: center;
+  margin: 1em;
+}
+.header h1{
+  margin: 0;
+}
+.header h1:hover{
+  color: #7f78d2;
+}
+</style>
---
+++ todolist/src/components/InputBox.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <form v-on:submit.prevent="addItem" class="input-box">
+      <input class="shadow" type="text" v-model="inputValue" v-on:Keyup="onKeyup" autofocus>
+      <button v-show="inputValue.length" v-on:click="onReset" type="reset" class="btn-reset"></button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['value'],
+  data() {
+    return {
+      inputValue: this.value
+    }
+  },
+  methods:{
+    addItem() {
+      this.$emit('@addItem', this.inputValue)
+      this.onReset()
+    },
+    onReset() {
+      this.inputValue = ''
+    },
+    onKeyup() {
+      if(!this.inputValue.length) this.onReset()
+    }
+  },
+}
+</script>
+
+<style>
+  .input-box {
+    position: relative;
+    margin: 0 auto;
+    width: 80%;
+  }
+  input {
+    border-style: none;
+    box-sizing: border-box;
+    width: 100%;
+    padding: 0.6em;
+  }
+  input:focus {
+    outline: none;
+  }
+  .btn-reset{
+    position: absolute;
+    border-radius: 50%;
+    background-color: #d3cff8;
+    color: white;
+    border: none;
+    padding: 2px 5px;
+    top: 7px;
+    right: 10px;
+  }
+  .btn-reset::before {
+    content: 'X';
+  }
+</style>
